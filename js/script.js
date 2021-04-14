eb9348fa7886a510813bc35d0be98852847941c4
Vladislav <vlad.skraimer@gmail.com>
2021-04-14 15:26:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Int
+let number = 4.6;
+// String
+const leftBorderWidth = 'Left border width';
+// Boolean
+let age = true;
+// Null
+//console.log(nol);
+// Undefined
+let und;
+console.log(und);
+// Symbol
+// BigInt
+// Object
+let obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+console.log(obj.name);
+
+// Array
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
+
+// Интерполяция
+const firstName = "Vladislav";
+console.log(`Hi, ${firstName}`);
+
+// Операторы
+console.log(2*4==='8');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || isClose);
