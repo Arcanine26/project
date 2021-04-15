081d49e9164ea8838fceb02794bb2b10b3229923
Vladislav <vlad.skraimer@gmail.com>
2021-04-15 14:14:36 +0300

    end on cycles

--- js/script.js
+++ js/script.js
@@ -1,36 +1,82 @@
-// Int
-let number = 4.6;
-// String
-const leftBorderWidth = 'Left border width';
-// Boolean
-let age = true;
-// Null
-//console.log(nol);
-// Undefined
-let und;
-console.log(und);
-// Symbol
-// BigInt
-// Object
-let obj = {
-    name: 'John',
-    age: 25,
-    isMarried: false
-};
-console.log(obj.name);
+// // ТИПЫ ДАННЫХ
+// // Int
+// let number = 4.6;
+// // String
+// const leftBorderWidth = 'Left border width';
+// // Boolean
+// let age = true;
+// // Null
+// //console.log(nol);
+// // Undefined
+// let und;
+// console.log(und);
+// // Symbol
+// // BigInt
+// // Object
+// let obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name);
+// // Array
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
 
-// Array
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-console.log(arr[1]);
+// // ИНТЕРПОЛЯЦИЯ
+// const firstName = "Vladislav";
+// console.log(`Hi, ${firstName}`);
 
-// Интерполяция
-const firstName = "Vladislav";
-console.log(`Hi, ${firstName}`);
+// // ОПЕРАТОРЫ
+// console.log(2*4==='8');
+// const isChecked = true,
+//       isClose = false;
+// console.log(isChecked || isClose);
 
-// Операторы
-console.log(2*4==='8');
+// // УСЛОВИЯ
+// if (4 == 4) {
+//     console.log('OK!');
+// } else {
+//     console.log('Fail!');
+// }
+// // Тернарный оператор
+// const num = 50;
+// (num === 50) ? console.log('OK!') : console.log('Fail!');
+// // Switch
+// switch(num) {
+//     case 49:
+//         console.log('Fail');
+//         break;
+//     case 100:
+//         console.log('Fail');
+//         break;
+//     case '50':
+//         console.log('OK!');
+//         break;
+//     default:
+//         console.log('Not today');
+//         break;
+// }
 
-const isChecked = true,
-      isClose = false;
-
-console.log(isChecked || isClose);
+// // Циклы
+// let num = 50;
+// while (num <= 55 ) {
+//     console.log(num);
+//     num++;
+// }
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+// for (let i = 1; i < 8; i++){
+//     console.log(num);
+//     num++;
+// }
+// for (let i = 1; i < 8; i++){
+//     if (i === 6){
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
